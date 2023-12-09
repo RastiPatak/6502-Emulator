9ea8e0de8298ec3fc59fdcd199e66c03ce72d741
RastiPatak <pat210600@spengergasse.at>
2023-12-09 20:25:30 +0100

    Created MOS6502 class

--- Uncem_6502/Main.cpp
+++ Uncem_6502/Main.cpp
@@ -1,50 +1,81 @@
 #include <iostream>
 
-static char registerX;
-static char registerY;
-static short programCounter;
+class MOS6502 {
+public:
+	MOS6502() 
+		: mAccumulator(0), mRegisterX(0), mRegisterY(0), mProgramCounter(0), mStackPointer(0xFD), C(0), Z(0), I(0), D(0), B(0), V(0), N(0) {
+		for (std::size_t i = 0; i < sizeof(mMemory) / sizeof(mMemory[0]); ++i) {
+			mMemory[i] = 0;
+		}
+	}
+
+	void loadProgram(const uint8_t* program, std::size_t size, uint16_t offset) {
+		std::size_t i = offset;
+		for (std::size_t j = 0; j < size; j++) {
+			mMemory[i++] = program[j];
+		}
+	}
+
+	void execute() {
+		while (true) {
+			uint8_t opcode = fetch();
+			executeOpcode(opcode);
+			// Implement other logic such as handling interrupts, etc.
+		}
+	}
+
+private:
+	uint8_t mMemory[65536];
+	uint8_t mAccumulator, mRegisterX, mRegisterY;
+	uint16_t mProgramCounter;
+	uint8_t mStackPointer;
+	uint8_t C, Z, I, D, B, V, N;
+
+	uint8_t fetch() {
+		uint8_t data = mMemory[mProgramCounter];
+		mProgramCounter++;
+		return data;
+	}
+
+	void executeOpcode(uint8_t opcode) {
+		switch (opcode)
+		{
+		case 0xE8:
+			mRegisterX++;
+			setZeroAndNegativeFlags(mRegisterX);
+			std::cout << "RegisterX: " << std::hex << (int)mRegisterX << std::endl;
+			break;
+		case 0xEA:
+			break;
+		default:
+			std::cerr << "Unknown opcode: " << std::hex << (int)opcode << std::endl;
+			std::exit(1);
+		}
+	}
+
+	void setZeroAndNegativeFlags(uint8_t value) {
+		Z = value == 0;
+		N = (value & 0x80) != 0;
+	}
+};
 
 int main()
 {
-	unsigned char c[65536];
-
-	c[0] = 0xEA;
-	c[1] = 0xEA;
-	c[2] = 0xEA;
-	c[3] = 0xE8;
-	c[4] = 0xEA;
-	c[5] = 0xE8;
-
-	bool isValid = true;
-	while (true)
+	uint8_t program[] =
 	{
-		switch (c[programCounter])
-		{
-		case 0xE8:
-			registerX++;
-			programCounter++;
-			break;
-		case 0xEA:
-			programCounter++;
-			break;
-		default:
-			isValid = false;
-			std::cout << "End of instructions at: " << programCounter << ", value: " << c[programCounter] << std::endl;
-			break;
-		}
-		if (!isValid)
-			break;
-	}
+		0xEA,
+		0xEA,
+		0xEA,
+		0xE8,
+		0xEA,
+		0xE8
+	};
 
-	std::cout << c[0] << std::endl;
-	std::cout << c << std::endl;
-	std::cout << "Hello! Yes, this message is utterly pointless, but don't judge me leaving it here." << std::endl;
+	MOS6502 cpu;
+	cpu.loadProgram(program, sizeof(program), 0x0000);
+	cpu.execute();
+
 	//for (const unsigned char& a : c)
 	//	std::cout << (a + 0) << ",";
 	//std::cout << std::endl;
-
-	std::cout << "Register X: " << (registerX + 0) << std::endl;
-
-	//char b;
-	//std::cin >> b;
 }
